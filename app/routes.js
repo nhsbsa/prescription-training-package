c56bdf9774fbc0d4320d6703566db1f7cae54b09
Shehnaz Katteth Salim <156828817+ShehnazKattethSalim@users.noreply.github.com>
2025-08-14 15:08:24 +0100

    admin-full

--- app/routes.js
+++ app/routes.js
@@ -6,3 +6,16 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+// Admin - checks-complete //
+
+router.post(/removeAnswerpool/, function (req, res) {
+
+  const dereg = req.session.data['removeAnswerpool']
+
+  if (dereg === "yes") {
+    res.redirect('/v1/admin/checks-complete/confirmation-removed-pool');
+  } else {
+    res.redirect('/v1/admin/checks-complete/error-review-2');
+  }
+});
